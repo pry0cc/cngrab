75623d95847c6fa2a5ed273163bbf4c73e845000
pry0cc <pry0cc@0x00sec.org>
2020-06-30 08:34:07 -0400

    Added cn scraper scripts, courtesy of hakluke

--- work/dumpCN.py
+++ work/dumpCN.py
@@ -7,3 +7,6 @@ cert = ssl.PEM_cert_to_DER_cert(cert) #Convert certificate to DER format
 begin = cert.rfind(b'\x06\x03\x55\x04\x03') + 7 #Find the last occurence of this byte string indicating the CN, add 7 bytes to startpoint to account for length of byte string and padding
 end = begin + cert[begin - 1] #Set endpoint to startpoint + the length of the CN
 print(cert[begin:end].decode('utf-8')) #Retrieve the CN from the DER encoded certificate
+
+
+# Originally written by @hakluke, included for the axiom box
